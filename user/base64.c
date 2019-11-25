d4b30d88805727f198911c63a3638c566bee010d
yangwensen <yangws@landicorp.com>
2019-11-25 16:56:55 +0800

    base64解码可以返回正确的编码前实际数据字节数

--- user/base64.c
+++ user/base64.c
@@ -24,7 +24,7 @@ static const int8_t TAB_BASE64_DECODE[128] =
     -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
     -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 
     -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 62, -1, -1, -1, 63,
-    52, 53, 54, 55, 56, 57, 58, 59, 60, 61, -1, -1, -1,  0, -1, -1,
+    52, 53, 54, 55, 56, 57, 58, 59, 60, 61, -1, -1, -1, -2, -1, -1,
     -1,  0,  1,  2,  3,  4,  5,  6,  7,  8,  9, 10, 11, 12, 13, 14, 
     15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, -1, -1, -1, -1, -1, 
     -1, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 
@@ -37,30 +37,39 @@ static int8_t base64_decrypt(const char * cbuf, char * pbuf)
     uint32_t temp = 0;
 	uint8_t i;
 	int8_t c;
+	uint8_t valid = 0;		//解码前的有效数据个数
 
     for(i=0; i<4; i++)
 	{
 		c = cbuf[i];
-		BASE64_DBG("[%d]", c);
+		BASE64_DBG("[%02X.", c);
 		if(c & 0x80)return -1;
 
 		c = TAB_BASE64_DECODE[c];
-		if(c<0)return -2;
-		
+		if(c==-1)return -2;
+		BASE64_DBG("%02X]", c);
+
 		temp <<= 6;
-		temp |= c;
+		if(c!=-2)			//get '='
+		{
+			temp |= c;
+			valid++;
+		}
     }
 
-	BASE64_DBG("---->");
+	BASE64_DBG("--%d-->", valid);
+	
+	if(valid<2)return -3;
 
-    for(i=0; i<3; i++)
+	valid--;		//解码有效字节数
+    for(i=0; i<valid; i++)
 	{
         pbuf[i] = (temp >> (16-(i<<3))) & 0xfful;
-		BASE64_DBG("[%d]", pbuf[i]);
+		BASE64_DBG("[%02X]", pbuf[i]);
     }
 	BASE64_DBG("\n");
 	
-	return 0;
+	return valid;
 }
 //***************************************************************************************
 //by yangwensen@20191125
@@ -69,6 +78,8 @@ extern int8_t base64_decode(char * buf, int len)
 	char *pbuf;
 	int plen;
 	int i;
+	uint32_t n = 0;
+	int8_t ret;
 
 	if(len & 3)return -1;
 
@@ -85,12 +96,14 @@ extern int8_t base64_decode(char * buf, int len)
 
     for(i=0; i < plen; i++)
 	{
-        if(base64_decrypt(&buf[i*4], &pbuf[i*3]) < 0);//return -3;
+		ret = base64_decrypt(&buf[i*4], &pbuf[i*3]);
+        if( ret < 0)return -3;
+		n += ret;
     }
 	rt_kprintf("before decode\r\n");
     memdump((uint8_t *)buf, len);
 	rt_kprintf("after decode\r\n");
-    memdump((uint8_t *)pbuf, plen*3);
+    memdump((uint8_t *)pbuf, n);
 
     rt_free(pbuf);
 	return plen*3;
@@ -102,8 +115,10 @@ void base64_test(void)
 {
 	const char BASE64_STR[] = "MTIzNDU2Nzg5MA==";
 	const char BASE64_STR2[] = "MTIzNDU2Nzg5MDk=";
+	const char BASE64_STR3[] = "MTIzNDU2Nzg5MDk4";
 
 	base64_decode((char *)BASE64_STR, sizeof(BASE64_STR)-1);
 	base64_decode((char *)BASE64_STR2, sizeof(BASE64_STR2)-1);
+	base64_decode((char *)BASE64_STR3, sizeof(BASE64_STR3)-1);
 }
 //***************************************************************************************
